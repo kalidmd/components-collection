f4abefb4531ff7433c6005d0738b466a5ed84002
Kalid Mohammed <110621255+kalidmd@users.noreply.github.com>
2024-01-22 00:03:20 +0300

    back to top button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import BackToTop from "./components/BackToTop";
 
 function App() {
   return (
     <div className="App">
-      hello
+      <BackToTop />
     </div>
   );
 }
